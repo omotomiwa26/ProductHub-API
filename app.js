bad5c8738dc4d9903a55d258addb2fb2075ee584
omotomiwa26 <omotomiwa26@gmail.com>
2025-01-25 04:26:48 +0100

    REFACTOR : reorganize middleware and route usage in app.js for improved clarity

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+
+
 const authRoutes = require("./routes/auth");
 const profileRoutes = require("./routes/profile");
 const productRoutes = require("./routes/product");
-const cors = require("cors");
+
 
 const app = express();
+
+
 app.use(bodyParser.json());
 app.use(cors());
-
 app.use(express.static(path.join(__dirname, "public")));
+
+
 app.set("view engine", "ejs");
+
+
+ // Use All Routes
+ app.use("/api/auth/v1", authRoutes);
+ app.use("/api/v1", profileRoutes);
+ app.use("/api/v1", productRoutes);
 
 // home route
 app.get("/", (req, res) => {
   res.render("landingPage");
-
-  // Use All Routes
-  app.use("/api/auth/v1", authRoutes);
-  app.use("/api/v1", profileRoutes);
-  app.use("/api/v1", productRoutes);
 });
 
 module.exports = app;
